8b4d80aadc42f26f9b2898738013fe2479911183
plombir1917 <dfhhggh@list.ru>
2025-01-14 15:34:38 +0300

    feat: prettir for logos

--- components/Layout/logo/LogoIcon.vue
+++ components/Layout/logo/LogoIcon.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="logo">
-        <NuxtLink to="/">
-            <img src="/images/logos/white-logo-icon.svg" alt="logo" />
-        </NuxtLink>
-    </div>
+  <div class="logo">
+    <NuxtLink to="/">
+      <img src="/images/logos/white-logo-icon.svg" alt="logo" />
+    </NuxtLink>
+  </div>
 </template>
--- components/Layout/logo/LogoLightIcon.vue
+++ components/Layout/logo/LogoLightIcon.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import { RouterLink } from "vue-router";
+import { RouterLink } from 'vue-router';
 </script>
 <template>
-    <div class="logo">
-      <NuxtLink to="/" class="d-flex">
-        <img src="/images/logos/white-logo-icon.svg" />
-      </NuxtLink>
-    </div>
-  </template>
-
-  
+  <div class="logo">
+    <NuxtLink to="/" class="d-flex">
+      <img src="/images/logos/white-logo-icon.svg" />
+    </NuxtLink>
+  </div>
+</template>
