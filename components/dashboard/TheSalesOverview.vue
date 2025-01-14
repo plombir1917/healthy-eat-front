7f5314bc187fc5c7b4acbb3b75ec0fa93ac5d3e9
plombir1917 <dfhhggh@list.ru>
2025-01-14 15:33:23 +0300

    feat: russian for sales

--- components/dashboard/TheSalesOverview.vue
+++ components/dashboard/TheSalesOverview.vue
@@ -1,35 +1,36 @@
 <script setup lang="ts">
-import { ref, onMounted,computed } from "vue";
+import { ref, onMounted, computed } from 'vue';
+
 /*Chart*/
 const chartOptions = computed(() => {
-    return {
-      grid: {
+  return {
+    grid: {
       show: false,
-      borderColor: "transparent",
+      borderColor: 'transparent',
       padding: { left: 0, right: 0, bottom: 0 },
     },
     plotOptions: {
-      bar: { horizontal: false, columnWidth: "35%", borderRadius: 0 },
+      bar: { horizontal: false, columnWidth: '35%', borderRadius: 0 },
     },
-    colors: ['#1e88e5','#0cb9c5'],
-    fill: { type: "solid", opacity: 1 },
+    colors: ['#1e88e5', '#0cb9c5'],
+    fill: { type: 'solid', opacity: 1 },
     chart: {
-      type: "bar",
+      type: 'bar',
       height: 320,
       offsetX: -15,
       toolbar: { show: false },
-      foreColor: "#adb0bb",
-      fontFamily: "Poppins",
+      foreColor: '#adb0bb',
+      fontFamily: 'Poppins',
       sparkline: { enabled: false },
     },
     dataLabels: { enabled: false },
     markers: { size: 0 },
     legend: { show: false },
     xaxis: {
-      type: "category",
-      categories: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      type: 'category',
+      categories: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
       labels: {
-        style: { cssClass: "grey--text lighten-2--text fill-color" },
+        style: { cssClass: 'grey--text lighten-2--text fill-color' },
       },
     },
     yaxis: {
@@ -39,23 +40,24 @@ const chartOptions = computed(() => {
       tickAmount: 3,
       labels: {
         style: {
-          cssClass: "grey--text lighten-2--text fill-color",
+          cssClass: 'grey--text lighten-2--text fill-color',
         },
       },
     },
     stroke: {
       show: true,
       width: 5,
-      lineCap: "butt",
-      colors: ["transparent"],
+      lineCap: 'butt',
+      colors: ['transparent'],
     },
-    tooltip: { theme: "dark" },
-    };
+    tooltip: { theme: 'dark' },
+  };
 });
+
 const Chart = {
   series: [
-    { name: "Ample", data: [355, 390, 300, 350, 390, 180, 250] },
-    { name: "Pixel", data: [280, 250, 325, 215, 250, 310, 170] },
+    { name: 'Ample', data: [355, 390, 300, 350, 390, 180, 250] },
+    { name: 'Pixel', data: [280, 250, 325, 215, 250, 310, 170] },
   ],
 };
 
@@ -69,11 +71,11 @@ onMounted(() => {
   <!-- ------------------------------------ -->
   <!-- html -->
   <!-- ------------------------------------ -->
-<VCard class="pa-3" elevation="10">
+  <VCard class="pa-3" elevation="10">
     <v-card-text>
       <div class="d-sm-flex align-center">
         <div>
-          <h3 class="text-h5 title mb-1">Sales Overview</h3>
+          <h3 class="text-h5 title mb-1">Обзор продаж</h3>
           <h5 class="text-subtitle-1">Ample Admin Vs Pixel Admin</h5>
         </div>
         <div class="ml-auto">
@@ -99,7 +101,10 @@ onMounted(() => {
       </div>
       <div v-show="elementVisible" class="mt-5">
         <apexchart
-        type="bar" height="400" :options="chartOptions" :series="Chart.series"
+          type="bar"
+          height="400"
+          :options="chartOptions"
+          :series="Chart.series"
         ></apexchart>
       </div>
     </v-card-text>
