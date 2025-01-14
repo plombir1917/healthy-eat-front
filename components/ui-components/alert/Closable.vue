a6c7142a77d4ef48ea87046a1f9f531b35c25d98
plombir1917 <dfhhggh@list.ru>
2025-01-14 15:36:20 +0300

    feat: russian and prittify for closable

--- components/ui-components/alert/Closable.vue
+++ components/ui-components/alert/Closable.vue
@@ -2,29 +2,24 @@
 import { ref } from 'vue';
 const alert = ref(true);
 </script>
+
 <template>
-   <div>
+  <div>
     <v-alert
       v-model="alert"
       border="start"
       variant="tonal"
       closable
-      close-label="Close Alert"
+      close-label="Закрыть уведомление"
       color="primary"
-      title="Closable Alert"
+      title="Закрываемое уведомление"
     >
-      Aenean imperdiet. Quisque id odio. Cras dapibus. Pellentesque ut neque. Cras dapibus.
-
-      Vivamus consectetuer hendrerit lacus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non
+      Aenean imperdiet. Quisque id odio. Cras dapibus. Pellentesque ut neque.
+      Cras dapibus. Vivamus consectetuer hendrerit lacus. Sed mollis, eros et
+      ultrices tempus, mauris ipsum aliquam libero, non
     </v-alert>
-    <div
-      v-if="!alert"
-    >
-      <v-btn
-        color="primary"
-        @click="alert = true" flat>
-        Reset
-      </v-btn>
+    <div v-if="!alert">
+      <v-btn color="primary" @click="alert = true" flat>Сбросить</v-btn>
     </div>
   </div>
 </template>
